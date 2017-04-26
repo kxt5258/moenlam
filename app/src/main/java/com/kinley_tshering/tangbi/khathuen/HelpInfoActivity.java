561655c03b2ee948b8c2ca9b9fbbaae3c0f53281
Kinley Tshering <kxt5258@gmail.com>
2017-04-27 00:33:31 +0600

    Made the links clickable

--- app/src/main/java/com/kinley_tshering/tangbi/khathuen/HelpInfoActivity.java
+++ app/src/main/java/com/kinley_tshering/tangbi/khathuen/HelpInfoActivity.java
@@ -2,6 +2,8 @@ package com.kinley_tshering.tangbi.khathuen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class HelpInfoActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class HelpInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_info);
+
+        //Make the links clickable
+        TextView help = (TextView) findViewById(R.id.help);
+        help.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView credit = (TextView) findViewById(R.id.credit);
+        credit.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
